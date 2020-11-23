8a3410d9c0bd71b18c3e353b131562457ee3a195
simun Yang <didtlans150@gmail.com>
2020-11-23 09:22:13 +0900

    저속이동과 저속이동할때 회전하는 이펙트 추가

--- 기말 프로젝트/player.py
+++ 기말 프로젝트/player.py
@@ -15,10 +15,14 @@ class Player:
         (SDL_KEYUP, SDLK_RIGHT):    (-1, 0)
     }
     KEYDOWN_Z = (SDL_KEYDOWN, SDLK_z)
+    KEYDOWN_SHIFT = (SDL_KEYDOWN, SDLK_LSHIFT)
     KEYUP_Z = (SDL_KEYUP, SDLK_z)
+    KEYUP_SHIFT = (SDL_KEYUP, SDLK_LSHIFT)
     SHOOTHING_INTERVAL = 0.05
+    ROTATING_SLOWEFF = 3.14 / 180
     def __init__(self):
         self.image = gfw.image.load('./res/player.png')
+        self.slowEffImage = gfw.image.load('./res/eff_sloweffect.png').clip_image(27,27,10,10) # 임시, 원래값: 0,0,64,64
         self.pos = get_canvas_width() // 2, 100
         self.delta = 0, 0
         self.fidx = 0
@@ -26,6 +30,8 @@ class Player:
         self.time = 0
         self.shooting_time = 0
         self.speed = 300
+        self.degree = 0
+        self.slowing = False
         self.shooting = False
 
 
@@ -48,12 +54,17 @@ class Player:
         if self.shooting and self.shooting_time > Player.SHOOTHING_INTERVAL:
             self.fire()
 
+        self.degree += Player.ROTATING_SLOWEFF
+
     def draw(self):
         width, height = 32, 48
         sx = self.fidx * width
         sy = (self.image.h - 48) - (self.action * height)
 
         self.image.clip_draw(sx, sy, width, height, *self.pos)
+
+        if self.slowing:
+            self.slowEffImage.rotate_draw(self.degree, *self.pos)
 
     def handle_event(self, e):
         pair = (e.type, e.key)
@@ -68,6 +79,13 @@ class Player:
             self.shooting = True
         elif pair == Player.KEYUP_Z:
             self.shooting = False
+
+        if pair == Player.KEYDOWN_SHIFT:
+            self.slowing = True
+            self.speed = 100
+        elif pair == Player.KEYUP_SHIFT:
+            self.slowing = False
+            self.speed = 300
 
 
     def fire(self):
